b1702a7a8c9f99249f8ad24db3fb0110fd0e87c3
SnehalNivsarkar <snehal.nivsarkar1@gmail.com>
2020-12-07 22:34:17 +0530

    crud based project

--- CRUDProject/settings.py
+++ CRUDProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')=mu10^yor#f1o3(a6=ijbrfc9)ug#hu-s&!#yno3)b3umo_0t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'CRUDProject.urls'
